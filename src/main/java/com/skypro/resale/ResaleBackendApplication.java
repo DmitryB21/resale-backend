efb400bdda641e401ac8884b211dee0a22c3f7b9
Dmitry Blyukherov <d.blukherov@gmail.com>
2023-07-03 17:46:44 +0300

    Initial commit

---
+++ src/main/java/com/skypro/resale/ResaleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.resale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResaleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResaleBackendApplication.class, args);
+    }
+
+}
